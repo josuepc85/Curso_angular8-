d8fd83a8e8f02ac810e292ce68e79bebe0bb9d53
josuepc85 <josuepc@conalepqroo.edu.mx>
2019-11-12 00:39:39 -0500

    Parte 2 del SPA

--- 02-spa/src/assets/img/app.module.ts
+++ 02-spa/src/assets/img/app.module.ts
@@ -7,28 +7,28 @@ import { HttpModule } from '@angular/http';
 import { APP_ROUTING } from './app.routes';
 
 // Servicios
-//import { HeroesService } from './servicios/heroes.service';
+import { HeroesService } from './servicios/heroes.service';
 
 // Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-//import { AboutComponent } from './components/about/about.component';
-//import { HeroesComponent } from './components/heroes/heroes.component';
-//import { HeroeComponent } from './components/heroe/heroe.component';
-//import { BuscadorComponent } from './components/buscador/buscador.component';
-//import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    //AboutComponent,
-   //HeroesComponent,
-    //HeroeComponent,
-    //BuscadorComponent,
-    //HeroeTarjetaComponent
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    BuscadorComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
