2c6ba67ebf67191c5fd0ca6b1d866e13ca151837
josuepc85 <josuepc@conalepqroo.edu.mx>
2019-11-21 16:06:28 -0500

    Rutas en angular

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -6,29 +6,25 @@ import { HttpModule } from '@angular/http';
 // Rutas
 import { APP_ROUTING } from './app.routes';
 
+
 // Servicios
-//import { HeroesService } from './servicios/heroes.service';
+
 
 // Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-//import { AboutComponent } from './components/about/about.component';
-//import { HeroesComponent } from './components/heroes/heroes.component';
-//import { HeroeComponent } from './components/heroe/heroe.component';
-//import { BuscadorComponent } from './components/buscador/buscador.component';
-//import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    //AboutComponent,
-   //HeroesComponent,
-    //HeroeComponent,
-    //BuscadorComponent,
-    //HeroeTarjetaComponent
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
@@ -36,9 +32,7 @@ import { HomeComponent } from './components/home/home.component';
     HttpModule,
     APP_ROUTING
   ],
-  providers: [
-    HeroesService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 02-spa/src/app/app.routes.ts
+++ 02-spa/src/app/app.routes.ts
@@ -1,18 +1,12 @@
-
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-/*import { AboutComponent } from './components/about/about.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroeComponent } from './components/heroe/heroe.component';
-import { BuscadorComponent } from './components/buscador/buscador.component';*/
+import { AboutComponent } from './components/about/about.component';
 
-const APP_ROUTES: Routes = [
+
+const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  //{ path: 'about', component: AboutComponent },
-  //{ path: 'heroes', component: HeroesComponent },
-  //{ path: 'heroe/:id', component: HeroeComponent },
-  //{ path: 'buscar/:termino', component: BuscadorComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: 'about', component: AboutComponent },
+  { path: '**', pathMatch:'full', redirectTo: 'home' }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
+export const APP_ROUTING = RouterModule.forRoot(ROUTES, { useHash:true });
