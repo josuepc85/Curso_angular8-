17eb5c3f22375740e7b9e86a50dd7ea938a3146e
josuepc85 <josuepc@conalepqroo.edu.mx>
2019-11-28 17:30:17 -0500

    Creando plantilla heroe

--- 02-spa/src/app/components/heroe/heroe.component.ts
+++ 02-spa/src/app/components/heroe/heroe.component.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute } from '@angular/router'
 
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
-
 })
 export class HeroeComponent {
 
-  heroe: any = {};
-
-  constructor( private ActivatedRoute: ActivatedRoute, private _heroesSerive: HeroesService
-
-    ) {
-    this.ActivatedRoute.params.subscribe(params => {
-      // console.log( params[' id ']);
-      this.heroe = this._heroeSerive.getHeroe( params['id']);
-    })
-   }
+  heroe:any = {};
 
 
+  constructor( private activatedRoute: ActivatedRoute,
+               private heroesService: HeroesService
+    ){
+
+    this.activatedRoute.params.subscribe( params =>{
+        this.heroe = this.heroesService.getHeroe( params['id'] );
+        // console.log(this.heroe);
+    });
+
+  }
 
 }
