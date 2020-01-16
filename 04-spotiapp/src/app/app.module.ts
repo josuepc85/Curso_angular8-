02aabc6e03fd253cc23e1aafca1d84f1f5fff4b7
josuepc85 <josuepc@conalepqroo.edu.mx>
2020-01-16 17:11:34 -0500

    Se agregaron los componentes de spotiapp

--- 04-spotiapp/src/app/app.module.ts
+++ 04-spotiapp/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
-import { SharedComponent } from './components/shared/shared.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+
+// Importar rutas
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    SearchComponent,
-    ArtistaComponent,
-    SharedComponent,
-    NavbarComponent
+      AppComponent,
+      HomeComponent,
+      SearchComponent,
+      ArtistaComponent,
+      NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
