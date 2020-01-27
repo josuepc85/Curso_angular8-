6c2822c00ca6555590d65e75de35eff58d386bc4
enripastrana <josuepastrana.c@gmail.com>
2020-01-27 15:21:54 -0500

    Inciando el search

--- 04-spotiapp/src/app/components/search/search.component.ts
+++ 04-spotiapp/src/app/components/search/search.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  artistas: any[] = [];
 
-  constructor() { }
+  constructor( private spotify: SpotifyService) { }
 
-  ngOnInit() {
+  buscar(termino: string) {
+    console.log(termino);
+    this.spotify.getArtista( termino )
+    .subscribe( (data: any) => {
+      console.log(data.artists.items);
+      this.artistas.data.artists.items;
+    });
   }
-
 }
--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -16,11 +16,13 @@ export class SpotifyService {
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers } );
-    /*
-    .subscribe ( data => {
-      console.log(data);
-    });*/
+  }
 
+  getArtista(termino: string){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCKOKvIq6Dobbqd2aBXSQZ1Q9mgncd2UHJ4lzBBsUSbRc2IW_5JQE94GMUIqR5vBY-kebdj1-jx0y7ucCvy5iYwRuZRqFKTq6q45hThDzgniW03oPAI5M13EBjCoq9QddDOUnhTgTeDlunCfbfRk8seMisayYGYc0A'
+    });
 
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers } );
   }
 }
